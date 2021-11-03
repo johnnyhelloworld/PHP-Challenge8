a9cfd5854306f625c9ef984206f63d88ebcfdf18
johnnyhelloworld <johnny58723826@hotmail.fr>
2021-11-03 13:15:31 +0100

    challenge9: sécurisation des champs

--- form.php
+++ form.php
@@ -2,23 +2,23 @@
 <form action="thanks.php" method="post">
     <div>
         <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="user_lastname">
+        <input type="text" id="lastname" name="user_lastname">
     </div>
     <div>
         <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="user_firstname">
+        <input type="text" id="firstname" name="user_firstname">
     </div>
     <div>
         <label for="courriel">Courriel :</label>
-        <input type="email" id="courriel" name="user_email">
+        <input type="email" id="email" name="user_email">
     </div>
     <div>
         <label for="telephone">Téléphone :</label>
-        <input type="text" id="telephone" name="user_phone">
+        <input type="text" id="phone" name="user_phone">
     </div>
     <div>
         <label for="sujet">Choisir un sujet :</label>
-        <select id="sujet" name="user_subject">
+        <select id="subject" name="user_subject">
             <option value="france">France</option>
             <option value="international">International</option>
             <option value="economie">Economie</option>
@@ -30,7 +30,7 @@
     </div>
     <div>
         <label for="message">Message :</label>
-        <textarea id="message"  name="user_message"></textarea>
+        <textarea id="message" name="user_message"></textarea>
     </div>
     <div class="button">
         <button type="submit">Envoyer votre message</button>
--- thanks.php
+++ thanks.php
@@ -1,6 +1,88 @@
 <?php
 
-echo "Merci " . $_POST["user_firstname"] . " " . $_POST["user_lastname"] . " de nous avoir contacté à propos de " . $_POST["user_subject"] . ".</br> </br>" .
-"Un de nos conseiller vous contactera soit à l'adresse " . $_POST["user_email"] . " ou par téléphone <br> au " 
-. $_POST["user_phone"] . " dans les plus brefs délais pour traiter votre demande: <br><br>" .
-$_POST["user_message"];
+//define variables and set to empty values
+$lastNameErr = $firstNameErr = $emailErr = $phoneErr = $subjectErr = $messageErr = "";
+$lastName = $firstName = $email = $phone = $subject = $message = "";
+
+function test_input($input)
+{
+    $input = stripslashes(trim(htmlspecialchars($input)));
+    return $input;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (empty($_POST["user_lastname"])){
+        $lastNameErr = "Last name is required";
+    } else {
+        $lastName = test_input($_POST["user_lastname"]);
+        // check if lastname only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$lastName)) {
+            $lastNameErr = "Only letters and white space allowed";
+        }
+    }
+
+    if (empty($_POST["user_firstname"])){
+        $firstNameErr = "First name is required";
+    } else {
+        $firstName = test_input($_POST["user_firstname"]);
+        // check if firstname only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$firstName)) {
+            $firstNameErr = "Only letters and white space allowed";
+        }
+    }
+
+    if (empty($_POST["user_email"])){
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["user_email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+        }
+    }
+
+    if (empty($_POST["user_phone"])){
+        $phoneErr = "Phone is required";
+    } else {
+        $phone = test_input($_POST["user_phone"]);
+        // check if phone is well-formed
+        if (!preg_match("/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/",$phone)){
+            $phoneErr = "Invalid phone format";
+        }
+    }
+
+    if (empty($_POST["user_subject"])){
+        $subjectErr = "Subject is required";
+    } else {
+        $subject = test_input($_POST["user_subject"]);
+    }
+
+    if (empty($_POST["user_message"])){
+        $messageErr = "Message is required";
+    } else {
+        $message = test_input($_POST["user_message"]);
+        // check if message only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$message)) {
+            $messageErr = "Only letters and white space allowed";
+        }
+    }
+}
+
+echo $lastNameErr;
+echo "</br>";
+echo $firstNameErr;
+echo "</br>";
+echo $emailErr;
+echo "</br>";
+echo $phoneErr;
+echo "</br>";
+echo $subjectErr;
+echo "</br>";
+echo $messageErr;
+
+if ($lastName && $firstName && $email && $phone && $subject && $message){
+    echo "Merci " . $_POST["user_firstname"] . " " . $_POST["user_lastname"] . " de nous avoir contacté à propos de " . $_POST["user_subject"] . ".</br> </br>" .
+    "Un de nos conseiller vous contactera soit à l'adresse " . $_POST["user_email"] . " ou par téléphone <br> au " 
+    . $_POST["user_phone"] . " dans les plus brefs délais pour traiter votre demande: <br><br>" .
+    $_POST["user_message"];
+}
